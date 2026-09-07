4b3c859e98c86721aa6954ca6113219c5614ee8c
yel-gar <231206328+yel-gar@users.noreply.github.com>
2026-09-07 14:55:50 +0300

    fix: alembic parser url

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -12,7 +12,7 @@ from app.util import get_database_url
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option("sqlalchemy.url", get_database_url())
+config.set_main_option("sqlalchemy.url", get_database_url().replace("%", "%%"))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
