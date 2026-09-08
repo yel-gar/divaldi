23e05477f224a2b2dafddd3fcba8e9e0c1834aa4
Vladislav <vladoqqik@icloud.com>
2026-09-08 08:59:07 +0300

    feat: add pages

---
+++ frontend/src/app/pages/chats-page/chats-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chats-page',
+  imports: [],
+  templateUrl: './chats-page.html',
+  styleUrl: './chats-page.scss'
+})
+export class ChatsPage {}
