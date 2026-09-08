7765e0427e9656a383bca2b286025a8bcb0fbd4b
yel-gar <231206328+yel-gar@users.noreply.github.com>
2026-09-08 07:59:06 +0300

    ci: correct pre-commit hooks for parser, dxf as binary in gitattributes

--- scripts/precommit-ruff.py
+++ scripts/precommit-ruff.py
@@ -1,7 +1,7 @@
 import subprocess
 import sys
 
-files = [f.removeprefix("backend/") for f in sys.argv[1:]]
+files = [f.removeprefix(sys.argv[1]) for f in sys.argv[2:]]
 
 subprocess.run(
     ["poetry", "run", "ruff", "check", "--fix", *files],
